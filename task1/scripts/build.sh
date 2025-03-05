ff55811e250a17da7aef1b93dd5bccdacaf9c839
Shlyapin Ilya <shlyapin2005@gmail.com>
2025-03-05 13:40:06 +0300

    Исправление работы с конфигами conan

--- task1/scripts/build.sh
+++ task1/scripts/build.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/bash
 
-set -x
-
-echo $USER
-getent passwd $USER | cut -d: -f6
-
-echo $HOME
-
-pwd
-
 conan config install https://github.com/isshlyapin/conan_config.git
 conan install . --output-folder=build --build=missing -s build_type=Release -pr:b gcc13 -pr:h gcc13
 cmake --preset conan-release
--- task1/scripts/setup_home.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-
-ln -s $(getent passwd $USER | cut -d: -f6) $HOME
