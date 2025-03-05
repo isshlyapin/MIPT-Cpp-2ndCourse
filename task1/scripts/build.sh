d31aa14578f075332461803def1948459cd1982a
Shlyapin Ilya <shlyapin2005@gmail.com>
2025-03-05 11:50:03 +0300

    Исправление scripts/build.sh

--- task1/scripts/build.sh
+++ task1/scripts/build.sh
@@ -2,6 +2,6 @@
 
 set -x
 
-conan install . --output-folder=build --build=missing --settings=build_type=Release -pr gcc-13
+conan install . --output-folder=build --build=missing -s build_type=Release -pr gcc-13
 cmake --preset conan-release
 cmake --build --preset conan-release
